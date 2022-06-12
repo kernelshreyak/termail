05983d166bd38a1e6e2a333d2903beef6ebec77e
Shreyak Chakraborty <shreyak.rekshda@gmail.com>
2022-06-12 12:12:42 +0530

    integrated inbox scraper

---
+++ core/scraper.py
@@ -0,0 +1,73 @@
+import asyncio
+from pyppeteer import launch
+from string import Template
+
+async def scrape_inbox(email):
+    try:
+        browser = await launch(headless=True)
+        page = await browser.newPage()
+        await page.goto('https://yopmail.com/en/')
+
+        data = await page.evaluate(Template('document.querySelectorAll("input.ycptinput")[0].value="$value"')
+            .substitute(value=email));
+
+        button = await page.querySelector('#refreshbut > button')        
+        await button.click()
+        await page.waitForNavigation();
+        # await page.screenshot({'path': 'output.png'})
+
+        # inbox is loaded so read all emails
+        data = await page.evaluate('''() => {
+                return Array.from(document.querySelectorAll("#ifinbox")[0].contentWindow.document.querySelectorAll
+                ("html > body > div.mctn > div.m")).map(elem => elem.innerText);
+            }
+        ''') 
+        # print(data)
+        
+        await browser.close()
+        return data
+    except Exception as e:
+        raise e
+    
+async def scrape_single_email(email,email_index):
+    try:
+        browser = await launch(headless=True)
+        page = await browser.newPage()
+        await page.goto('https://yopmail.com/en/')
+
+        await page.evaluate(Template('document.querySelectorAll("input.ycptinput")[0].value="$value"')
+            .substitute(value=email));
+
+        button = await page.querySelector('#refreshbut > button')        
+        await button.click()
+        await page.waitForNavigation();
+        # await page.screenshot({'path': 'output.png'})
+
+        # inbox is loaded so read all emails
+        emailtext = await page.evaluate(Template('''() => {
+                const elems =  Array.from(document.querySelectorAll("#ifinbox")[0].contentWindow.document.querySelectorAll
+                ("html > body > div.mctn > div.m"));
+                
+                elems[$index].click();
+
+                return document.querySelectorAll("#ifmail")[0].contentWindow.document.querySelectorAll
+                ("html > body > main")[0].innerText;
+            }
+        ''').substitute(index=email_index)) 
+        # print(data)
+        
+        await browser.close()
+        return emailtext
+    except Exception as e:
+        raise e
+
+
+def read_inbox(email):
+    return asyncio.get_event_loop().run_until_complete(scrape_inbox(email))
+    # return ["Email 1","Email 1","Email 1","Email 1"]
+
+def read_single_email(email,index):
+    return asyncio.get_event_loop().run_until_complete(scrape_single_email(email,index))
+
+# print(read_inbox("shreyak"))
+# print(read_single_email("shreyak",2))
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import urwid
+from core.scraper import *
 
 choices = ["View Inbox","Send Email","Exit"]
 
@@ -16,13 +17,30 @@ def item_chosen(button, choice):
         return
 
     elif choice == choices[0]:  ## view inbox
-        # TODO: show emails from inbox
-        pass
+        emails = read_inbox("termail_001")
+
+        inboxheader = urwid.Text([u'Inbox'])
+        email_disp = [inboxheader]
+        for email in emails:
+            email_disp.append(urwid.AttrMap(urwid.Button(email), None, focus_map='reversed'))
+        
+        back_button = urwid.Button('Exit')
+        urwid.connect_signal(back_button, 'click', back_to_mainmenu )
+        email_disp.append(urwid.AttrMap(back_button, None, focus_map='reversed'))  
+
+        main.original_widget = urwid.Filler(urwid.Pile(email_disp))
+
 
     elif choice == choices[1]:  ## send email
         # TODO: in a new window, show form to create email
         pass
 
+def back_to_mainmenu(button):
+    main.original_widget =  urwid.Overlay(urwid.Padding(menu(u'Termail v1.0', choices), left=2, right=2), 
+        urwid.SolidFill(u'\N{MEDIUM SHADE}'),
+        align='center',width=180,height=90,
+        valign='middle',
+        min_width=20, min_height=9)
 
 def exit_program():
     raise urwid.ExitMainLoop()
@@ -32,4 +50,5 @@ top = urwid.Overlay(main, urwid.SolidFill(u'\N{MEDIUM SHADE}'),
     align='center', width=('relative', 180),
     valign='middle', height=('relative', 90),
     min_width=20, min_height=9)
+
 urwid.MainLoop(top, palette=[('reversed', 'standout', '')]).run()
